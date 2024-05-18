2cd66ffa97e616dc2602af0aeec0cb8b2522b543
YxngXi1 <yang@xue.email>
2024-05-17 21:19:42 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById('nameForm').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    var userName = document.getElementById('name').value;
+
+    if (userName === "Rebecca Lu" || userName == "123youareweird")  {
+        console.log("Access granted");
+        window.location.href = "secondslide.html";
+    } else {
+        console.log("Access denied");
+    }
+});
